13c53980dec4d115e4287d8494c25246f83a66ca
Tjarco Kerssens <info@tjarcokerssens.nl>
2019-07-15 10:21:43 +0200

    Initial Commit

---
+++ Entree Federatie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Entree Federatie
+//
+//  Created by Tjarco Kerssens on 15/07/2019.
+//  Copyright © 2019 Kennisnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Entree FederatieTests/Entree_FederatieTests.swift
@@ -0,0 +1,34 @@
+//
+//  Entree_FederatieTests.swift
+//  Entree FederatieTests
+//
+//  Created by Tjarco Kerssens on 15/07/2019.
+//  Copyright © 2019 Kennisnet. All rights reserved.
+//
+
+import XCTest
+@testable import Entree_Federatie
+
+class Entree_FederatieTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
