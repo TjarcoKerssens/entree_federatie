f4628ff6877823e3e1d9d46a76bca62f5626b8ee
Tjarco Kerssens <info@tjarcokerssens.nl>
2019-07-18 20:53:51 +0200

    Init WikiWijs target for showing SSO
        * Created an application target
        * Added logo
        * Configured code sharing
        * Configured Pod
        * Added WebView

---
+++ WikiWijs/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WikiWijs
+//
+//  Created by Tjarco Kerssens on 18/07/2019.
+//  Copyright © 2019 Kennisnet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
